c9fa5aef7e35fe0cff6c28b8a08c0c0c49b94f74
Filip Axani <filip.axani@gmail.com>
2013-11-09 00:55:47 +0100

    Adding swedish

--- js/maincontroller.js
+++ js/maincontroller.js
@@ -89,6 +89,10 @@ app.controller("MainController", function($scope, $http){
          'turkish': {
             'letters': ['ç', 'ö', 'ü', 'ą', 'ć', 'ę', 'ğ', 'ı', 'ł', 'ń', 'ś', 'ş', 'ź', 'ż', 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
             'name': 'turkish'
+        },
+         'swedish': {
+            'letters': ['ä', 'å', 'ç', 'ö', 'ü', 'ą', 'ć', 'ę', 'ğ', 'ı', 'ł', 'ń', 'ś', 'ş', 'ź', 'ż', 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
+            'name': 'swedish'
         }
 
     }
